4c50e89c4384d5f52f72790f8f534b0cf22c2bd1
Sandra Adey <sandraadeyinka@gmail.com>
2023-06-16 13:09:26 +0100

    Updated

--- Kata.py
+++ Kata.py
@@ -1,12 +1,4 @@
 #Reversing words in a string
 #Hello everyone = everyone hello
-
-import string
-
-
-def reverse(st):
-    lst = string.split(" ")
-    lst.reverse()
-    reversed_string = " ".join(lst)
-    print(reversed_string)
-
+def reverse(str):
+    return " ".join(reversed(str.split())).split()
